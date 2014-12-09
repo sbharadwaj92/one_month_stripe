3d6d3ef844c924f109ac777d51389136040e175c
Santosh Bharadwaj <sbharadwaj92@yahoo.co.in>
2014-12-09 23:08:37 +0530

    fixed UI

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,24 +1,22 @@
 class ChargesController < ApplicationController
 
   def create
-  # Amount in cents
-  @amount = 500
 
-  customer = Stripe::Customer.create(
-    :email => 'example@stripe.com',
-    :card  => params[:stripeToken]
-  )
+    customer = Stripe::Customer.create(
+      :email => params[:stripeEmail],
+      :card  => params[:stripeToken]
+    )
 
-  charge = Stripe::Charge.create(
-    :customer    => customer.id,
-    :amount      => @amount,
-    :description => 'Rails Stripe customer',
-    :currency    => 'usd'
-  )
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => params[:amount],
+      :description => 'Growth Hacking Crash Course',
+      :currency    => 'usd'
+    )
 
-rescue Stripe::CardError => e
-  flash[:error] = e.message
-  redirect_to charges_path
-end
-  
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to charges_path
+  end
+
 end
